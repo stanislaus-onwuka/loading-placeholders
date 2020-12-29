f5ab3998ad1fc7e3f9144be1d1618f83ee96a086
Onwuka Stanley <kachi2505@yahoo.com>
2020-12-29 17:37:55 +0100

    Styling update

--- dist/placeholder/placeholder.js
+++ dist/placeholder/placeholder.js
@@ -3,15 +3,47 @@ import './placeholder.css';
 
 const Placeholder = ({
   height,
-  width
+  width,
+  br,
+  margin
 }) => {
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "placeholder",
     style: {
       height: height,
-      width: width
+      width: width,
+      borderRadius: br,
+      margin: margin
     }
   }));
 };
 
-export default Placeholder;
+const Placeholders = ({
+  height,
+  width,
+  br,
+  n,
+  margin
+}) => {
+  const loading = (pHeight, pWidth, pBorderRadius, pNumber) => {
+    let placeholders = [];
+
+    for (let i = 0; i < Number(pNumber); i++) {
+      placeholders.push( /*#__PURE__*/React.createElement(Placeholder, {
+        height: `${pHeight}`,
+        width: `${pWidth}`,
+        br: `${pBorderRadius}`,
+        margin: `${margin}`,
+        key: i
+      }));
+    }
+
+    return placeholders;
+  };
+
+  return /*#__PURE__*/React.createElement("div", {
+    className: "placeholders"
+  }, loading(height, width, br, n));
+};
+
+export default Placeholders;
